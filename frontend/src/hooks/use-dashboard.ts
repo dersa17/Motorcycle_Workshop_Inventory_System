1445a64910a6e9f46837206fc46520353e146812
dersa17 <dersasembiring11@gmail.com>
2025-12-20 09:59:25 +0700

    feat(dashboard-frontend): set refetch to always on window focus and mount

--- frontend/src/hooks/use-dashboard.ts
+++ frontend/src/hooks/use-dashboard.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import * as dashboardService from "@/services/dashboard-service"
-
+    
 
 export const useDashboard = () => {
     return useQuery({
             queryKey: ["dashboard"],
             queryFn: dashboardService.getDataDashboard,
             staleTime: 1000 * 60 * 30,
-            refetchOnWindowFocus: true,
+            refetchOnWindowFocus: "always",
+            refetchOnMount: "always"
     })
 }
